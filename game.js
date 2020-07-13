c4e8051d79779a46b34876772c3dd6f093e178e1
codeofdreams <67717331+codeofdreams@users.noreply.github.com>
2020-07-13 16:24:28 -0500

    Update game.js

--- game.js
+++ game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem("mostRecentStore", score);
 
-    return window.location.assign("/end.html");
+    return window.location.assign("./end.html");
   }
 
   questionCounter++;
